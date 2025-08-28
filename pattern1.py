fa6f8d84d5b6e476d67fdcbd144272416ecd7394
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-28 22:05:54 +0530

    Update pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -1,8 +1,14 @@
-for i in range(4):  
-    print("* " * 5)  
+row = 4
+column = 5
+for i in range(row):
+    l = ''
+    for j in range(column):
+        l = l + '* '  
+    print(l)
     
     
 #* * * * * 
 #* * * * * 
 #* * * * * 
+
 #* * * * * 
