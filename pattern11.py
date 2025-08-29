df394a6a73b4fdad2be18ef7ebe859570d6fc78e
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-29 07:21:57 +0530

    Update pattern11.py

--- pattern11.py
+++ pattern11.py
@@ -1,12 +1,14 @@
-n = 5  
+n=5
 for i in range(n):
-    star = n - i
-    space = i * 2 + 1
-    print('*' * star + ' ' * space + '*' * star)
-for i in range(n):
-    star = i + 1
-    space = (n - i - 1) * 2 + 1
-    print('*' * star + ' ' * space + '*' * star)
+    print("*" * (n-i) ,end="")
+    print(" " * (i*2) , end="")  
+    print("*" * (n-i) )
+n=5
+for i in range(1,n+1):
+    print("*" * (i) ,end="")
+    print(" " * ((2*n)-(i*2)) , end="")  
+    print("*" * (i) )
+
 
 
 
@@ -19,4 +21,5 @@ for i in range(n):
 #**                ** 
 #***           *** 
 #****      **** 
+
 #**********    
