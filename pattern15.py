c4b9450fb8ab1167723de1fc23bee3dcc4fc32aa
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-29 06:48:37 +0530

    Update pattern15.py

--- pattern15.py
+++ pattern15.py
@@ -1,8 +1,13 @@
-n = 5 
-for i in range(1, n + 1):
-    print('*' * i)
-for i in range(n - 1, 0, -1):
-    print('*' * i)
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()    
+n=4
+for i in range(n):
+    for j in range(i,n):
+        print("*" , end=" ")
+    print()      
 
 
 
@@ -16,3 +21,4 @@ for i in range(n - 1, 0, -1):
 #*** 
 #** 
 #* 
+
