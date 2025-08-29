284bc8fc8874e37a489d53bafe3b81ed45d2b326
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-29 07:58:48 +0530

    Update pattern7.py

--- pattern7.py
+++ pattern7.py
@@ -1,8 +1,11 @@
-row = 5
-for i in range(1, row + 1):
-    l = row - i
-    print(' ' * l, end='')  
-    print('* ' * i)
+n=int(input("enter the number "))
+for i in range(1,n+1):
+     for j in range (n-i):
+         print("_", end=" ")
+     for k in range(i):
+         print("*",end=" ")
+     print()
+   
 
 
 
@@ -14,3 +17,4 @@ for i in range(1, row + 1):
 # _  * *  * * 
 
 #  *  * *  * *   
+
