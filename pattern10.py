d06f47bdb3bd3cfc8299bd9763e3405b869f04d2
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-29 07:18:50 +0530

    Update pattern10.py

--- pattern10.py
+++ pattern10.py
@@ -1,15 +1,14 @@
 
-n = 5  
-for i in range(1, n + 1):
-    print('*' * i, end='')
-    space = (n - i) * 2
-    print(' ' * space, end='')
-    print('*' * i)
-
+n=5
+for i in range(1,n+1):
+    print("*" * (i) ,end="")
+    print(" " * ((2*n)-(i*2)) , end="")  
+    print("*" * (i) )
 
 
 #*                     * 
 #**                ** 
 #***           *** 
 #****      **** 
+
 #**********
